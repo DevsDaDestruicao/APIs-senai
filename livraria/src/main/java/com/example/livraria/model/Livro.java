50ce5f712be79f2d1905b07f750886aac19e7a11
Daniel Dos Santos Bittencourt <NIT0024423@aluno.firjansenaisesi.com.br>
2025-06-30 21:22:03 -0300

    Primeiro commit

---
+++ livraria/src/main/java/com/example/livraria/model/Livro.java
@@ -0,0 +1,19 @@
+package com.example.livraria.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity // Indica que essa classe é uma entidade JPA, ou seja, será mapeada para uma tabela no banco de dados
+public class Livro {
+
+    @Id // Indica que esse campo é uma chave primaria da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Define que o valor do ID será gerado automaticamente pelo banco de dados
+    private Long id;
+
+    private String titulo;
+    private String autor;
+    private int ano;
+
+
+}
