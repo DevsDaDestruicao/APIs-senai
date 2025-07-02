859b22f9873dc5a306f6e4925bb1283e149a61ef
Daniel Dos Santos Bittencourt <NIT0024423@aluno.firjansenaisesi.com.br>
2025-07-01 21:13:12 -0300

    Foi add repository, service, dto

--- livraria/src/main/java/com/example/livraria/service/LivroService.java
+++ livraria/src/main/java/com/example/livraria/service/LivroService.java
@@ -1,4 +1,53 @@
 package com.example.livraria.service;
 
+import com.example.livraria.dto.LivroDTO;
+import com.example.livraria.model.Livro;
+import com.example.livraria.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class LivroService {
+    @Autowired
+    private LivroRepository livroRepository;
+
+    private LivroDTO toDTO(Livro livro) {
+        LivroDTO dto = new LivroDTO();
+        dto.setId(livro.getId());;
+        dto.setTitulo(livro.getTitulo());
+        dto.setAutor(livro.getAutor());
+        dto.setAno(livro.getAno());
+        return dto;
+    }
+
+    private Livro toEntity(LivroDTO dto) {
+        Livro livro = new Livro();
+        livro.setId(dto.getId());
+        livro.setTitulo(dto.getTitulo());
+        livro.setAutor(dto.getAutor());
+        livro.setAno(dto.getAno());
+        return livro;
+    }
+
+    public List<LivroDTO> listar() {
+        return livroRepository.findAll().stream().map(this::toDTO).collect(Collectors.toList());
+    }
+    public Optional<LivroDTO> buscar(Long id) { return livroRepository.findById(id).map(this::toDTO); }
+
+    public LivroDTO salvar(LivroDTO dto) {
+        Livro livro = toEntity(dto);
+        return toDTO(livroRepository.save(livro));
+    }
+
+    public LivroDTO atualizar(Long id, LivroDTO dto) {
+        dto.setId(id);
+        Livro livro = toEntity(dto);
+        return toDTO(livroRepository.save(livro));
+    }
+
+    public void deletar(Long id) {livroRepository.deleteById(id);}
 }
